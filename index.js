cbd34b89f13d20aaac2b5d9d2e839a161d604bab
Amil P A <amilpa2020@gmail.com>
2023-04-06 20:00:06 +0530

    final

--- index.js
+++ index.js
@@ -5,12 +5,53 @@ const User = require('./user')
 
 mongoose.connect("mongodb://localhost/testdb")
 
-const user = new User({ name : "Kyle" , age: 18})
-user.save()
+// const user = new User({ name : "Kyle" , age: 18})
+// user.save()
 
 async function run(){
-	const user = new User({ name : "Amil" , age : 26 })
-	await user.save()
+	try {
+		// const user = new User({
+		// 	name: "Jose",
+		// 	age: 26,
+		// 	email: "Theworld2003@gmail.com",
+		// 	hobbies: ["Weight lifting","Bowling"],
+		// 	address : {
+		// 		street : "Main st"
+		// 	}
+		// })
+		// await user.save()
+		// console.log(user)
+		// await User.deleteMany({})
+
+		//const user = await User.find()
+
+		//const user = await User.where("name").equals("Kyle")
+
+		// const user = new User({ name : "Subhash" , age: 20 , email : "amilpa2016@gmail.com"})
+
+		// const user = await User.where("name")
+		// 	.equals("Neeyetha")
+		// 	.limit(1)
+		// 	.populate("bestFriend")
+
+		// const user = await User.find().byName("Neeyetha")
+
+		const user = await User.where("name").equals("Rajesh").limit(1)
+		
+		user[0].name = "Rajesh"
+		user[0].save()
+		console.log(user)
+		// user[0].sayHi()
+
+		// const user = new User({ name : "Neeyetha" , age : "22" })
+
+		// user[0].bestFriend = "642eb75caf3bd22d9324b787"
+		// user[0].save()
+		// user.save()
+	}
+	catch (e) {
+		console.log(e.message)
+	}
 }
 
 run()
--- user.js
+++ user.js
@@ -1,13 +1,72 @@
 const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema({
-	name : String , 
-	age : Number
+const addressSchema = new mongoose.Schema({
+	street : String,
+	city : String
 })
 
 
+const userSchema = new mongoose.Schema({
+	name : String , 
+	age : {
+		type : Number, 
+		min : 1,
+		max : 100,
+		validate : {
+			validator : v => v%2 === 0,
+			message : props => `${props.value} is not an even number`
+		}
+	},
+	email : {
+		type : String , 
+		minLength : 10,
+		lowercase : true
+		// uppercase too works
+	},
+	createdAt : {
+		type : Date,  
+		default : () => Date.now(),
+		immutable : true
+	},
+	updatedAt : {
+		type : Date , 
+		default : () => Date.now()
+	},
+	bestFriend:{
+		type : mongoose.SchemaTypes.ObjectId,
+		ref : "User"
+	},
+	hobbies : [String],
+	address : addressSchema
+})
+
+userSchema.methods.sayHi = function(){
+	console.log(`Hi , My name is ${this.name}`)
+	throw new Error("stop him")
+} 
+
+userSchema.statics.findByName = function(name){
+	return this.where({ name : new RegExp(name , "i")}) // where can take same arguements as find too
+}
+
+userSchema.query.byName = function (name){ //apply to a query
+	return this.where({ name : new RegExp(name , "i")})
+}
+
+userSchema.virtual('namedEmail').get(function(){
+	return `${this.name} and ${this.email}`
+})
+
+// userSchema.pre('save' , function(next){
+// 	this.updatedAt = Date.now()
+// 	this.sayHi()
+// 	next()
+// })
+
+// userSchema.post('save', function(doc , next){
+// 	doc.sayHi()
+// 	next()
+// })
+
+
 module.exports = mongoose.model("User" , userSchema)
-
-
-
-
